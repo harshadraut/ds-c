fa77b4b7d8a39a4d4d62865fcebb73cffee2483f
Sai Manoj Kumar Yadlapati <ysaimanojkumar@gmail.com>
2013-09-16 17:44:33 +0530

    Creates a pointer to stack_t instead of a variable

--- src/dsdemo.c
+++ src/dsdemo.c
@@ -15,22 +15,23 @@ int main(int argc, char** argv) {
 
         printf("DS Demo\n");
         printf("Stack Demo\n");
-        stack_t s;
-        stack_init(&s);
+        stack_t *s;
+        s = malloc(sizeof(stack_t));
+        stack_init(s);
         int i;
 
         for(i = 1; i <= 10; i++){
-                if( 0 == stack_push(&s, i) ){
+                if( 0 == stack_push(s, i) ){
                         printf("Pushed %d onto stack\n", i);
-                        printf("Length of stack: %d\n", stack_len(&s));
+                        printf("Length of stack: %d\n", stack_len(s));
                 }
         }
 
         for(i = 1; i <= 10; i++){
-                printf("Popped %d from stack\n", stack_pop(&s));
-                printf("Length of stack: %d\n", stack_len(&s));
+                printf("Popped %d from stack\n", stack_pop(s));
+                printf("Length of stack: %d\n", stack_len(s));
         }
-
+        free(s);
         return (EXIT_SUCCESS);
 }
 
