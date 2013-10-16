1c66e668dd4dd6fbaf7b1cca9d9de7d596aaa0be
Sai Manoj Kumar Yadlapati <ysaimanojkumar@gmail.com>
2013-10-17 02:18:27 +0530

    Queue: Impl. destroy, reinit, resize

--- src/queue.c
+++ src/queue.c
@@ -43,3 +43,44 @@ int queue_len(const queue_t *q)
                 return len + q->capacity + 1;
 }
 
+int queue_destroy(queue_t *q)
+{
+        free(q->data);
+        q->data = NULL;
+        return 0;
+}
+
+int queue_reinit(queue_t *q)
+{
+        q->front = 0;
+        q->rear = 0;
+        return 0;
+}
+
+int queue_resize(queue_t *q, int new_capacity)
+{
+/* For resizing queue, elements should be moved if they are wrapping around the end
+ */
+        int old_capacity = q->capacity;
+        if( new_capacity < old_capacity ){
+                return -1;
+                /* Not Implemented Yet */
+        }else if(new_capacity == old_capacity){
+                return 0;
+        }
+        q->capacity = new_capacity;
+        q->data = realloc(q->data, ( q->capacity + 1 ) * sizeof(int));
+        if( q->front <= q->rear ){
+                if( q->data != NULL || q->capacity == 0 ){
+                        return 0;
+                }else{
+                        return -1;
+                }
+        }
+        int i;
+        for(i = 0; i < q->rear; i++){
+                q->data[(old_capacity+i+1) % new_capacity] = q->data[i];
+        }
+        q->rear = (old_capacity + i + 1) % new_capacity;
+}
+
--- src/queue.h
+++ src/queue.h
@@ -13,5 +13,9 @@ int queue_enqueue(queue_t *q, int v);
 int queue_dequeue(queue_t *q, int *v);
 int queue_len(const queue_t *q);
 
+int queue_destroy(queue_t *q);
+int queue_reinit(queue_t *q);
+int queue_resize(queue_t *q, int new_capacity);
+
 #endif /* QUEUE_H */
 
