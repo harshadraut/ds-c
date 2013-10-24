50ef95ea3827a5ea261fede161eb17daf3ce7b4a
Sai Manoj Kumar Yadlapati <ysaimanojkumar@gmail.com>
2013-10-24 14:52:07 +0530

    stack: Adds test using check for int push, pop

--- tests/check_stack.c
+++ tests/check_stack.c
@@ -1,39 +1,63 @@
+#include<stdlib.h>
+#include<check.h>
 #include"../src/stack.h"
 
+START_TEST(int_push_pop)
+{
+        stack_t s;
+        int n = random() % 60267;
+        int *a = calloc(n, sizeof (int));
+        ck_assert_int_eq(stack_init(&s, n), 0);
+        int i;
+        for (i = 0; i < n; i++) {
+                a[i] = random() % 1210211;
+        }
+
+        for (i = 0; i < n; i++) {
+                int x = stack_push(&s, a[i]);
+                ck_assert_int_eq(x, 0);
+                x = stack_len(&s);
+                ck_assert_int_eq(x, i + 1);
+                x = s.data[s.top];
+                ck_assert_int_eq(x, a[i]);
+        }
+
+        for (i = n - 1; i >= 0; i--) {
+                int value;
+                int x = stack_pop(&s, &value);
+                ck_assert_int_eq(x, 0);
+                ck_assert_int_eq(value, a[i]);
+                x = stack_len(&s);
+                ck_assert_int_eq(x, i);
+        }
+        stack_clear(&s);
+        stack_destroy(&s);
+}
+
+END_TEST
+
+Suite* stack_suite()
+{
+        Suite *s = suite_create("Stack");
+
+        TCase *tc_int = tcase_create("int");
+        /* Stack int data type Test Case*/
+
+        tcase_add_test(tc_int, int_push_pop);
+
+        suite_add_tcase(s, tc_int);
+
+        return s;
+}
 
 int main()
 {
-        stack_t s;
-        if( stack_init(&s, 10) == 0 ){
-//                printf("Stack Initialized\n");
-        }else{
-                printf("Stack Initialization Failed\n");
-        }
+        int number_failed;
+        Suite *s = stack_suite();
+        SRunner *sr = srunner_create(s);
+        srunner_run_all(sr, CK_NORMAL);
+        number_failed = srunner_ntests_failed(sr);
+        srunner_free(sr);
 
-        int i;
-
-        for(i = 1; i <= 10; i++){
-                if( 0 == stack_push(&s, i) ){
-//                        printf("Pushed %d onto stack\n", i);
-//                        printf("Length of stack: %d\n", stack_len(&s));
-//                        printf("%d is top of the stack\n", s.data[s.top]);
-                }else{
-                        printf("Unable to push %d\n", i);
-                }
-
-        }
-
-        for(i = 1; i <= 10; i++){
-                int value;
-                if ( stack_pop(&s, &value) == 0 ){
-//                        printf("Popped %d from stack\n", value);
-//                        printf("Length of stack: %d\n", stack_len(&s));
-                }else{
-                        printf("Unable to pop\n");
-                }
-        }
-        stack_destroy(&s);
-        stack_clear(&s);
-        return 0;
+        return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
-
