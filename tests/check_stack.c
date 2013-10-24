2c27d5d605235b5dedc123ecddbe7e235e3c6f42
Sai Manoj Kumar Yadlapati <ysaimanojkumar@gmail.com>
2013-10-25 01:01:59 +0530

    stack: Adds Test which expect generic functionality

--- tests/check_stack.c
+++ tests/check_stack.c
@@ -36,6 +36,44 @@ START_TEST(int_push_pop)
 
 END_TEST
 
+START_TEST(float_push_pop)
+{
+        stack_t s;
+        int n = random() % 60267;
+        float *a = calloc(n, sizeof (float));
+        ck_assert_int_eq(stack_init(&s, n), 0);
+        int i;
+        for (i = 0; i < n; i++) {
+                a[i] = (random() % 1210211) / (1.0 + random() / 23129.0);
+        }
+
+        for (i = 0; i < n; i++) {
+                int x = stack_push(&s, a[i]);
+                ck_assert_int_eq(x, 0);
+                x = stack_len(&s);
+                ck_assert_int_eq(x, i + 1);
+                float value;
+                value = stack_top(&s);
+                char s[100];
+                sprintf(s, "value is %f, a[i] is %f\n", value, a[i]);
+                ck_assert_msg(value == a[i], s);
+        }
+
+        for (i = n - 1; i >= 0; i--) {
+                float value;
+                int x = stack_pop(&s, &value);
+                ck_assert_int_eq(x, 0);
+                char s[100];
+                sprintf(s, "value is %f, a[i] is %f\n", value, a[i]);
+                ck_assert_msg(value == a[i], s);
+                x = stack_len(&s);
+                ck_assert_int_eq(x, i);
+        }
+        stack_destroy(&s);
+}
+
+END_TEST
+
 Suite* stack_suite()
 {
         Suite *s = suite_create("Stack");
@@ -43,9 +81,14 @@ Suite* stack_suite()
         TCase *tc_int = tcase_create("int");
         /* Stack int data type Test Case*/
 
+        TCase *tc_float = tcase_create("float");
+        /* Stack float data type Test Case*/
+
         tcase_add_test(tc_int, int_push_pop);
+        tcase_add_test(tc_float, float_push_pop);
 
         suite_add_tcase(s, tc_int);
+        suite_add_tcase(s, tc_float);
 
         return s;
 }
